1c291dab5d315dc0dcf2e1bf80de476fd81dd77d
ReforgedDream <reforgeddream@gmail.com>
2017-08-31 19:45:03 +0700

    Initial commit

---
+++ src/com/example/helloworld/WeatherParser.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class WeatherParser implements WeatherParserInterface {
+
+    @Override
+    public int getTemperature(String city) {
+        return 0;
+    }
+
+}
---
+++ src/com/example/helloworld/WeatherParserInterface.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public interface WeatherParserInterface {
+
+    int getTemperature(String city);
+
+}
