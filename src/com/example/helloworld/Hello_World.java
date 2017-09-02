1a1661fc992f40c1cf35819d231bf087bdaa98e5
ReforgedDream <reforgeddream@gmail.com>
2017-09-02 19:34:01 +0700

    Input from console was added.

--- src/com/example/helloworld/Hello_World.java
+++ src/com/example/helloworld/Hello_World.java
@@ -1,23 +1,31 @@
 package com.example.helloworld;
 
+import java.util.Scanner;
+
 public class Hello_World {
 
     static WeatherParser parser1 = new WeatherParser();
 
-    static private void printTempInCity(String inputCity){
+    //method that prints the human-readable message about weather
+    static private void printWeatherInCity(String inputCity){
 
+        //call the WeatherParser's class getTemperature
+        //and print it to standard output along with some human-readable notes
         System.out.println("Temperature in "+ inputCity + " is " + parser1.getTemperature(inputCity));
     }
 
     public static void main(String[] args) {
 
-        String currentCity = "Novosibirsk";
+        Scanner in = new Scanner(System.in);
 
-        printTempInCity(currentCity);
+        //write prompt in console
+        System.out.println("Enter city name:");
 
-        currentCity = "Tokyo";
+        //get typed the walue in currentCity
+        String currentCity = in.next();
 
-        printTempInCity(currentCity);
+        //call the method that prints the human-readable message about weather
+        printWeatherInCity(currentCity);
 
     }
 
